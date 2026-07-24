26b25b77a0a545e8cf0f20043b2d82f454f8ff32
likhitha chilumula <likhitha1004@gmail.com>
2026-07-24 11:52:54 +0000

    api key

--- CropSense-AI-Backend/routes/voice.py
+++ CropSense-AI-Backend/routes/voice.py
@@ -6,12 +6,10 @@ import os
 router = APIRouter()
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-
-
 for m in genai.list_models():
     print(m.name)
 # Temporary model (we'll replace it after seeing the logs)
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 
 class VoiceRequest(BaseModel):
