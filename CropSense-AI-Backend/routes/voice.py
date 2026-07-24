c03883fbd0441945ef066e5ee58287fd9c884c6a
likhitha chilumula <likhitha1004@gmail.com>
2026-07-24 10:32:21 +0000

    gemini

--- CropSense-AI-Backend/routes/voice.py
+++ CropSense-AI-Backend/routes/voice.py
@@ -7,7 +7,7 @@ router = APIRouter()
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 
 class VoiceRequest(BaseModel):
